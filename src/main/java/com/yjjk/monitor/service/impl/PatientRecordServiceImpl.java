e10bb765c26c104b8a4e1e5b34bc080e31d07d64
CentreS <CentreSeek@126.com>
2019-08-04 19:45:34 +0800

    修改设备预热时间

--- src/main/java/com/yjjk/monitor/service/impl/PatientRecordServiceImpl.java
+++ src/main/java/com/yjjk/monitor/service/impl/PatientRecordServiceImpl.java
@@ -73,8 +73,8 @@ public class PatientRecordServiceImpl extends BaseService implements PatientReco
             for (int j = 0; j < temperatureList.size(); j++) {
                 if (monitorList.get(i).getMachineId() == temperatureList.get(j).getMachineId()) {
                     Long recordTime = DateUtil.timeDifferentLong(monitorList.get(i).getStartTime(), DateUtil.getCurrentTime());
-                    // 监测时间小于10分钟为预热中
-                    if (recordTime <= 10) {
+                    // 监测时间小于2分钟为预热中
+                    if (recordTime <= 2) {
                         monitorList.get(i).setRecordState(MonitorRecord.RECORD_STATE_READY);
                     } else {
                         monitorList.get(i).setRecordState(MonitorRecord.RECORD_STATE_USAGE);
